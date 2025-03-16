88d5a9943c3b90d8f532a9901401cbd1c56ed447
Shivam Roy <royshivam0123@gmail.com>
2025-03-16 11:33:32 +0530

    Add Loading Animation, Update Footer, Guidelines

--- client/src/section/Policies/Policies.jsx
+++ client/src/section/Policies/Policies.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import styles from './styles/policies.module.scss';
 import PolicyCard from '../../component/PolicyCard/PolicyCard'; // Importing PolicyCard
 import policyData from '../../../public/data/policyItem.json'; // Importing policy data
-
 import policyImage from '../../assets/policy.jpg'; // Importing the image
+import { motion } from 'framer-motion';
 
 const Policies = () => {
   return (
     <div>
       <img src={policyImage} alt="Policies" className={styles['policy-image']} />
 
-      <div className={styles["policy-container"]}>
+      <motion.div className={styles["policy-container"]}
+         whileInView={{ opacity: 1, x: 0 }}
+         initial={{ opacity: 0, x: -30 }}
+         transition={{ type: "spring", stiffness: 100, duration: 0.5 }}
+      >
 
 
         {policyData.map((item, index) => (
           <PolicyCard key={index} heading={item.heading} link={item.link} />
         ))}
 
-      </div>
+      </motion.div>
     </div>
   )
 }
