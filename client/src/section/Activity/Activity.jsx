b417dd541cdd74b5b26a2f05cd3faf0ad729928c
Ayush995712 <ayush181217@gmail.com>
2025-07-03 07:25:36 +0530

    Update activities and corresponding styles

--- client/src/section/Activity/Activity.jsx
+++ client/src/section/Activity/Activity.jsx
@@ -7,16 +7,36 @@ import InnovationApply from '../../component/InnovationIdea/Idea';
 
 const activities = [
     {
-        title: "Promoting Innovation",
-        details: "This includes encouraging experimentation, supporting risk-taking, and providing resources and opportunities for individuals and teams to explore new concepts. By valuing diverse perspectives and staying open to change, organizations and societies can drive progress and achieve groundbreaking solutions."
+        title: "Internet of Medical Things (IoMT)",
+        details: "The Internet of Medical Things (IoMT) is a network of interconnected medical devices and sensors that collect and transmit healthcare data for monitoring and treatment purposes."
     },
     {
-        title: "Entrepreneurship Development",
-        details: "This includes offering access to funding, business advice, and practical tools, as well as creating a nurturing environment that encourages risk-taking and growth. Effective support helps entrepreneurs navigate challenges and scale their businesses, driving economic and social impact."
+        title: "Artificial Intelligence in Health care",
+        details: "Artificial Intelligence (AI) in healthcare refers to the use of advanced algorithms and data analytics to improve medical diagnosis, treatment, and patient outcomes."
     },
     {
-        title: "Technology Transfer & Research",
-        details: "It includes cultivating a collaborative community, facilitating access to funding and talent, and promoting an entrepreneurial culture. A well-developed ecosystem enhances the chances of startup success and drives innovation and economic growth."
+        title: "Bio-fluidics",
+        details: "Bio-fluidics is the study and manipulation of fluids and their properties in biological systems, with applications in fields such as biotechnology and medical diagnostics."
+    },
+    {
+        title: "Wearable Implants",
+        details: "Wearable implants are implantable medical devices that can be worn on or inside the body to monitor, track, or treat various health conditions."
+    },
+    {
+        title: "Mobile Health (mHealth)",
+        details: "Mobile health (mHealth) refers to the use of mobile devices and wireless technologies to support healthcare delivery and health outcomes."
+    },
+    {
+        title: "3D Printing",
+        details: "3D printing is a manufacturing process that creates three-dimensional objects by layering materials on top of each other based on a digital model, with applications in fields such as healthcare, engineering, and architecture."
+    },
+    {
+        title: "3D Imaging, AR, VR",
+        details: "3D imaging, augmented reality (AR), and virtual reality (VR) technologies create immersive, interactive experiences that enable users to visualize and interact with three-dimensional digital objects or environments, with applications in fields such as gaming, entertainment, education, and healthcare."
+    },
+    {
+        title: "Computer Aided Diagnostics (CAD)",
+        details: "Computer-aided diagnostics (CAD) involves the use of computer algorithms and machine learning techniques to support medical professionals in the interpretation of diagnostic imaging and other medical data for improved accuracy and efficiency."
     }
 ];
 
@@ -25,7 +45,7 @@ const Activity = () => {
         <div className={styles.mainAcitivity} id="activity">
             <div className={styles.heading}>
                 <p className={styles.head}>
-                    IIC'S{" "}
+                    <div>FOCUSED AREAS</div>
                     <span
                         style={{
                             background: "var(--primary)",
@@ -33,13 +53,13 @@ const Activity = () => {
                             color: "transparent",
                         }}
                     >
-                        Activity
+                    Focused areas of our<br />
+                    Incubation Centre
                     </span>
                 </p>
                 <div className={styles.bottomLine}></div>
             </div>
             <div className={styles.activityContainer}>
-                <div className={styles.details}>The following are key activities involved in the Institution's Innovation Council (IIC):</div>
                 <div className={styles.Activity}>
                     {activities.map((activity, index) => (
                         <motion.div
