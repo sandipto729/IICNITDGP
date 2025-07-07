361de8f4e1842b8df2fb4a9b86a39682151edfa8
Quantum-HG <hgquantum24@gmail.com>
2025-07-07 23:18:28 +0530

    Brought Appply Now portion to the top

--- client/src/section/Activity/Activity.jsx
+++ client/src/section/Activity/Activity.jsx
@@ -55,7 +55,7 @@ const Activity = () => {
                         </motion.div>
                     ))}
                 </div>
-                <InnovationApply />
+                {/* <InnovationApply /> */}
             </div>
         </div>
 
